e280ab0862feddd6e9763ff8c9ae66dc416b3c8d
tubone <meitantei-conan@live.jp>
2021-02-23 03:32:33 +0000

    security fix

--- src/component/avatar.jsx
+++ src/component/avatar.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 
-const ph = 'data:image/gif;base64,R0lGODlhAQABAGAAACH5BAEKAP8ALAAAAAABAAEAAAgEAP8FBAA7'
-
 export default ({ src, className, alt, defaultSrc = '//cdn.jsdelivr.net/npm/gitalk@1/src/assets/icon/github.svg' }) => (
   <div className={`gt-avatar ${className}`}>
-    <img className="lozad" src={ph} data-src={src || defaultSrc} alt={`@${alt}`} onError={function (e) {
+    <img src={src || defaultSrc} alt={`@${alt}`} onError={function (e) {
       e.target.src = defaultSrc
     }} />
   </div>
